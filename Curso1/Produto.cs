93bb7c70ceedc3f020a47271a3345a0f80c2e36e
pdsfilho <p.dantas@outlook.com>
2021-05-20 23:30:53 -0300

    Criando Repositório

---
+++ Curso1/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso1
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2")
+            + ", "
+            + Quantidade
+            + " unidades. Total: $ "
+            + ValorTotalEmEstoque().ToString("F2");
+        }
+    }
+}
---
+++ Curso1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Curso1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.WriteLine("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
